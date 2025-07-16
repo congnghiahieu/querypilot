4605748159b40b54461ed0720cf974e9ada91fb6
congnghiahieu <hieucn.work@gmail.com>
2025-07-16 22:32:42 +0700

    fix check rds

--- backend/check_rds.py
+++ backend/check_rds.py
@@ -3,15 +3,26 @@ from sqlmodel import Session, create_engine, text
 
 from src.core.settings import APP_SETTINGS
 
-# ✅ 2. Tạo engine
-engine = create_engine(
-    APP_SETTINGS.get_database_url, echo=True, connect_args={"sslmode": "require"}
-)
 
-# ✅ 3. Dùng session kiểm tra kết nối
-try:
-    with Session(engine) as session:
-        result = session.exec(text("SELECT 1;"))
-        print("RDS OK:", result.one())
-except SQLAlchemyError as e:
-    print("RDS ERROR:", str(e))
+def get_engine():
+    return create_engine(
+        APP_SETTINGS.get_database_url,
+        echo=True,
+        connect_args={"sslmode": "require"},
+    )
+
+
+def check_rds_connection():
+    engine = get_engine()
+    print("▶️ Checking DB:", APP_SETTINGS.get_database_url)
+
+    try:
+        with Session(engine) as session:
+            result = session.exec(text("SELECT 1;"))
+            print("✅ RDS OK:", result.one())
+    except SQLAlchemyError as e:
+        print("❌ RDS ERROR:", str(e))
+
+
+if __name__ == "__main__":
+    check_rds_connection()
